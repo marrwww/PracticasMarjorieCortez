b6af2c1124032150b8fa0f9cab4f3bbeb4ec395f
Maar <avelarmajo31@gmail.com>
2026-06-09 21:09:36 -0600

    react1

---
+++ practica1React/js/script.js
@@ -0,0 +1,29 @@
+const boton = document.getElementById('btn-generar');
+const inputJuego = document.getElementById('juego');
+const inputCalificacion = document.getElementById('calificacion');
+const textoResultado = document.getElementById('resultado');
+
+
+const obtenerVeredicto = (nota) => {
+if (nota >= 8) {
+return "¡Literalmente una obra maestra, es cine!";
+} else if (nota >= 5) {
+return "Decente, sirve para pasar el rato :/";
+} else {
+return "Que borre la cuenta mejor x_x";
+}
+};
+
+boton.addEventListener('click', () => {
+momento
+let nombreJuego = inputJuego.value;
+let nota = Number(inputCalificacion.value);
+if (nombreJuego === "" || inputCalificacion.value === "") {
+textoResultado.innerText = "Oye tío, pon un nombre y una nota válida.";
+return;
+}
+
+let veredictoFinal = obtenerVeredicto(nota);
+textoResultado.innerText = `Mi veredicto sobre "${nombreJuego}" es un 
+${nota}/10. ${veredictoFinal}`;
+});
